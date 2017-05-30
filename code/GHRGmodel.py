cd635ce6fdb8452649a08793c3f15ca39b082b7d
Michael Schaub <mschaub@mit.edu>
2017-05-30 18:34:06 -0400

    restructure GHRG

--- code/GHRGmodel.py
+++ code/GHRGmodel.py
@@ -41,7 +41,42 @@ triangular and include #undirected links or #directed links
 If the node is not a leaf node in D, then the diagonal entry in Nr / Er should be 0 by convention; since the precise details of the connectivity pattern are to be read out at a lower level
 
 """
+
+
 class GHRG(nx.DiGraph):
+
+    ##
+    ## FIRST PART -- MANIPULATIONS OF DENDROGRAM DATA STRUCTURE
+    ##
+
+    """
+    Generator for new node numbers
+    NB: Assumes node label 'v' indicates that the node was the vth node added
+    """
+    def new_node_generator(self):
+        v=len(self)
+        while True:
+            yield v
+            v += 1
+
+    """
+    Adds n_children to parent
+    Returns list of new nodes
+    """
+    def add_children(self, parent, n_children):
+        new_nodes=[]
+        new_labels=self.new_node_generator()
+        for nc in xrange(n_children):
+
+            # generate new node
+            new_node = new_labels.next()
+            self.add_node(new_node, Er=np.zeros((n_children,n_children)), Nr=np.zeros((n_children,n_children)))
+            new_nodes.append(new_node)
+
+            #generate new edge
+            self.add_edge(parent, new_node)
+
+        return new_nodes
 
     """
     Function to update the N_r and E_r parameters using a fixed probability matrix, omega
@@ -82,194 +117,17 @@ class GHRG(nx.DiGraph):
                     self.node[node]['Nr'] = (np.array([[self.node[node]['n']*self.node[node]['n']]])- self.node[node]['n'])/2
                     self.node[node]['Er'] = np.array([[self.node[node]['Nr'][0,0] * omega[level-1][ci,ci]]])
 
-
-
-    """
-    Generator for new node numbers
-    NB: Assumes node label 'v' indicates that the node was the vth node added
-    """
-    def new_node_generator(self):
-        v=len(self)
-        while True:
-            yield v
-            v += 1
-
-    """
-    Adds n_children to parent
-    Returns list of new nodes
-    """
-    def add_children(self, parent, n_children):
-        new_nodes=[]
-        new_labels=self.new_node_generator()
-        for nc in xrange(n_children):
-
-            # generate new node
-            new_node = new_labels.next()
-            self.add_node(new_node, Er=np.zeros((n_children,n_children)), Nr=np.zeros((n_children,n_children)))
-            new_nodes.append(new_node)
-
-            #generate new edge
-            self.add_edge(parent, new_node)
-
-        return new_nodes
-
-    """
-    Function to merge list of nodes in dendrogram and insert a new node in the hierarchy
-    """
-    def insert_hier_merge_node(self,node_ids):
-
-        ##################################################
-        # 1) preallocate information stored in merged node
-        joint_nnodes = np.empty(0)
-        joint_n = 0
-        n_blocks = len(node_ids)
-
-        # Check that all nodes are mergeable, i.e., have the same parent node
-        parent_id = self.predecessors(node_ids[0])[0]
-        ids = np.empty(len(node_ids),dtype='int')
-        for counter, node in enumerate(node_ids):
-            # check consistency of merger
-            if self.predecessors(node)[0] != parent_id:
-                print "These two nodes / blocks cannot be merged!"
-                return False
-
-            # get indices for Nr and Er arrays
-            ids[counter] = self.node[parent_id]['children'].index(node)
-
-            joint_nnodes = np.append(joint_nnodes,self.node[node]['nnodes'])
-            joint_n = joint_n + self.node[node]['n']
-
-        # read out info from old nodes and create union / joint
-        joint_Nr = self.node[parent_id]['Nr'][np.ix_(ids,ids)]
-        joint_Er = self.node[parent_id]['Er'][np.ix_(ids,ids)]
-
-        old_children = self.node[parent_id]['children']
-        pvec = np.zeros(len(self.node[parent_id]['children']),dtype='int')
-
-        num_new_children = len(old_children) - len(node_ids)
-        k=0
-        for ni, old_child in enumerate(old_children):
-            if old_child in node_ids:
-                pvec[ni] = num_new_children
-            else:
-                pvec[ni] = k
-                k = k+1
-
-
-        ##################################################
-        # create new node and insert joint info
-        new_label = self.new_node_generator()
-        new_id = new_label.next()
-        self.add_node(new_id)
-        self.node[new_id]['nnodes'] = joint_nnodes
-        self.node[new_id]['n'] = joint_n
-        self.node[new_id]['Er'] = joint_Er
-        self.node[new_id]['Nr'] = joint_Nr
-
-        # let new node point to old two nodes
-        self.node[new_id]['children'] = node_ids
-        for node in node_ids:
-            self.add_edge(new_id,node)
-
-
-        ##################################################
-        # update parent node info and let it point to new node
-        for node in node_ids:
-            self.remove_edge(parent_id,node)
-        self.add_edge(parent_id,new_id)
-
-        self.node[parent_id]['children']=self.successors(parent_id)
-        self.node[parent_id]['children'].sort()
-
-        # Check if this is doing the correct agglomeration here.
-        pmatrix = create_partition_matrix_from_vector(pvec).toarray()
-        A = self.node[parent_id]['Nr']
-        A = pmatrix.T.dot(A).dot(pmatrix)
-        self.node[parent_id]['Nr'] = A - np.diag(np.diag(A))
-
-        A = self.node[parent_id]['Er']
-        A = pmatrix.T.dot(A).dot(pmatrix)
-        self.node[parent_id]['Er'] = A - np.diag(np.diag(A))
-
-
-
+    def setLeafNodeOrder(self):
     """
     Function to identify and store leaf nodes (i.e. dendro nodes with no children) in
     internal data structure
     """
-    def setLeafNodeOrder(self):
-        self.leaf_nodes = [v for v in nx.dfs_preorder_nodes(self,self.root_node) if self.out_degree(v)==0]
-
-    """
-    Function to generate networks from the model
-    """
-    def generateNetworkBeta(self,mode='Undirected'):
-        return self.generateNetwork(edgeProb='beta')
-
-    """
-    Function to generate networks from the model
-    """
-    def generateNetworkExactProb(self,mode='Undirected'):
-        return self.generateNetwork(edgeProb='exact')
-
-    def generateNetwork(self,edgeProb='beta'):
-        """
-        Network nodes at each leaf of the dendro are equivalent.  For each leaf work out the
-        probability of connection with other blocks by working up to the root of the tree.
-        """
-        if self.directed:
-            error('directed case not defined yet')
-        else:
-            # create new graph and make sure that all nodes are added
-            # even though the graph might be disconnected
-            G=nx.Graph()
-            G.add_nodes_from(np.arange(self.node[0]['n']))
+        self.leaf_nodes = [v for v in nx.dfs_preorder_nodes(self, self.root_node) if self.out_degree(v) == 0]
 
 
-        # cycle through nodes and generate edges
-        for v in self.nodes_iter():
-
-            children=self.node[v]['children']
-            Nr=self.node[v]['Nr']
-            Er=self.node[v]['Er']
-            # NOTE: if Nr / Er matrices contain non-zero diagonals, then we create too many edges due to the leaf nodes.
-            # possible alternative is to ignore leaf nodes here in the sampling
-            # this might be the cleaner solution
-
-            for ci,cj in izip(*Nr.nonzero()):
-                try:
-                    childi=self.node[children[ci]]
-                    childj=self.node[children[cj]]
-                except IndexError:      # if it is a leaf node
-                    childi=self.node[v]
-                    childj=self.node[v]
-                try:
-                    if edgeProb=='beta':
-                        alpha=np.ones(Nr[ci,cj])+Er[ci,cj]
-                        beta=np.ones(Nr[ci,cj])+(Nr[ci,cj]-Er[ci,cj])
-                        p = np.random.beta(alpha,beta)
-                    elif edgeProb=='exact':
-                        p = Er[ci,cj]/Nr[ci,cj]
-                    else :
-                        error('edge probabilities undefined')
-                    # print "Probability"
-                    # print ci, cj, p
-                except ValueError:
-                    print "Something went wrong when sampling from the model"
-
-                # create edges to insert
-                edges = (np.random.rand(childi['n']*childj['n'])< p).reshape((childi['n'],childj['n']))
-                if ci == cj and not self.directed:
-                    edges = np.triu(edges)
-                edges = edges.nonzero()
-
-                G.add_edges_from(zip(childi['nnodes'][edges[0]],childj['nnodes'][edges[1]]))
-
-        #remove self loops
-        if not self.self_loops:
-            G.remove_edges_from(G.selfloop_edges())
-
-        return G
+    ##
+    ## PART 2 --- OUTPUT INFORMATION STORED IN DENDROGRAM
+    ##
 
     def generateNetwork_parameters(self,directed=False):
         """
@@ -397,16 +255,175 @@ class GHRG(nx.DiGraph):
     def detectability_report(self):
         pass
 
-# TODO this function should be phased out
-def create_partition_matrix_from_vector(partition_vec):
-    """
-    Create a partition indicator matrix from a given vector; -1 entries in partition vector will
-    be ignored and can be used to denote unasigned nodes.
-    """
-    nr_nodes = partition_vec.size
-    k=len(np.unique(partition_vec))
+    ##
+    ## PART 3 --- SAMPLING FUNCTIONS
+    ## TODO: externalize or keep internal?
 
-    partition_matrix = scipy.sparse.coo_matrix((np.ones(nr_nodes),(np.arange(nr_nodes), partition_vec)),shape=(nr_nodes,k)).tocsr()
-    return partition_matrix
+    def generateNetworkBeta(self,mode='Undirected'):
+        """
+        Function to generate networks from the model
+        """
+        return self.generateNetwork(edgeProb='beta')
+
+    def generateNetworkExactProb(self,mode='Undirected'):
+        """
+        Function to generate networks from the model
+        """
+        return self.generateNetwork(edgeProb='exact')
+
+    def generateNetwork(self,edgeProb='beta'):
+        """
+        Network nodes at each leaf of the dendro are equivalent.  For each leaf work out the
+        probability of connection with other blocks by working up to the root of the tree.
+        """
+        if self.directed:
+            error('directed case not defined yet')
+        else:
+            # create new graph and make sure that all nodes are added
+            # even though the graph might be disconnected
+            G=nx.Graph()
+            G.add_nodes_from(np.arange(self.node[0]['n']))
+
+
+        # cycle through nodes and generate edges
+        for v in self.nodes_iter():
+
+            children=self.node[v]['children']
+            Nr=self.node[v]['Nr']
+            Er=self.node[v]['Er']
+            # NOTE: if Nr / Er matrices contain non-zero diagonals, then we create too many edges due to the leaf nodes.
+            # possible alternative is to ignore leaf nodes here in the sampling
+            # this might be the cleaner solution
+
+            for ci,cj in izip(*Nr.nonzero()):
+                try:
+                    childi=self.node[children[ci]]
+                    childj=self.node[children[cj]]
+                except IndexError:      # if it is a leaf node
+                    childi=self.node[v]
+                    childj=self.node[v]
+                try:
+                    if edgeProb=='beta':
+                        alpha=np.ones(Nr[ci,cj])+Er[ci,cj]
+                        beta=np.ones(Nr[ci,cj])+(Nr[ci,cj]-Er[ci,cj])
+                        p = np.random.beta(alpha,beta)
+                    elif edgeProb=='exact':
+                        p = Er[ci,cj]/Nr[ci,cj]
+                    else :
+                        error('edge probabilities undefined')
+                    # print "Probability"
+                    # print ci, cj, p
+                except ValueError:
+                    print "Something went wrong when sampling from the model"
+
+                # create edges to insert
+                edges = (np.random.rand(childi['n']*childj['n'])< p).reshape((childi['n'],childj['n']))
+                if ci == cj and not self.directed:
+                    edges = np.triu(edges)
+                edges = edges.nonzero()
+
+                G.add_edges_from(zip(childi['nnodes'][edges[0]],childj['nnodes'][edges[1]]))
+
+        #remove self loops
+        if not self.self_loops:
+            G.remove_edges_from(G.selfloop_edges())
+
+        return G
+
+
+    ###
+    ### PART 4 --- INFERENCE FUNCTIONS (call external function)
+    ###
+    #TODO
+
+
+# TODO these functions are to be phased out
+    # """
+    # Function to merge list of nodes in dendrogram and insert a new node in the hierarchy
+    # """
+    # def insert_hier_merge_node(self,node_ids):
+
+        # ##################################################
+        # # 1) preallocate information stored in merged node
+        # joint_nnodes = np.empty(0)
+        # joint_n = 0
+        # n_blocks = len(node_ids)
+
+        # # Check that all nodes are mergeable, i.e., have the same parent node
+        # parent_id = self.predecessors(node_ids[0])[0]
+        # ids = np.empty(len(node_ids),dtype='int')
+        # for counter, node in enumerate(node_ids):
+            # # check consistency of merger
+            # if self.predecessors(node)[0] != parent_id:
+                # print "These two nodes / blocks cannot be merged!"
+                # return False
+
+            # # get indices for Nr and Er arrays
+            # ids[counter] = self.node[parent_id]['children'].index(node)
+
+            # joint_nnodes = np.append(joint_nnodes,self.node[node]['nnodes'])
+            # joint_n = joint_n + self.node[node]['n']
+
+        # # read out info from old nodes and create union / joint
+        # joint_Nr = self.node[parent_id]['Nr'][np.ix_(ids,ids)]
+        # joint_Er = self.node[parent_id]['Er'][np.ix_(ids,ids)]
+
+        # old_children = self.node[parent_id]['children']
+        # pvec = np.zeros(len(self.node[parent_id]['children']),dtype='int')
+
+        # num_new_children = len(old_children) - len(node_ids)
+        # k=0
+        # for ni, old_child in enumerate(old_children):
+            # if old_child in node_ids:
+                # pvec[ni] = num_new_children
+            # else:
+                # pvec[ni] = k
+                # k = k+1
+
+
+        # ##################################################
+        # # create new node and insert joint info
+        # new_label = self.new_node_generator()
+        # new_id = new_label.next()
+        # self.add_node(new_id)
+        # self.node[new_id]['nnodes'] = joint_nnodes
+        # self.node[new_id]['n'] = joint_n
+        # self.node[new_id]['Er'] = joint_Er
+        # self.node[new_id]['Nr'] = joint_Nr
+
+        # # let new node point to old two nodes
+        # self.node[new_id]['children'] = node_ids
+        # for node in node_ids:
+            # self.add_edge(new_id,node)
+
+
+        # ##################################################
+        # # update parent node info and let it point to new node
+        # for node in node_ids:
+            # self.remove_edge(parent_id,node)
+        # self.add_edge(parent_id,new_id)
+
+        # self.node[parent_id]['children']=self.successors(parent_id)
+        # self.node[parent_id]['children'].sort()
+
+        # # Check if this is doing the correct agglomeration here.
+        # pmatrix = create_partition_matrix_from_vector(pvec).toarray()
+        # A = self.node[parent_id]['Nr']
+        # A = pmatrix.T.dot(A).dot(pmatrix)
+        # self.node[parent_id]['Nr'] = A - np.diag(np.diag(A))
+
+        # A = self.node[parent_id]['Er']
+        # A = pmatrix.T.dot(A).dot(pmatrix)
+        # self.node[parent_id]['Er'] = A - np.diag(np.diag(A))
+# def create_partition_matrix_from_vector(partition_vec):
+    # """
+    # Create a partition indicator matrix from a given vector; -1 entries in partition vector will
+    # be ignored and can be used to denote unasigned nodes.
+    # """
+    # nr_nodes = partition_vec.size
+    # k=len(np.unique(partition_vec))
+
+    # partition_matrix = scipy.sparse.coo_matrix((np.ones(nr_nodes),(np.arange(nr_nodes), partition_vec)),shape=(nr_nodes,k)).tocsr()
+    # return partition_matrix
 
 
