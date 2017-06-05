0f41ea0ff8511b17b9c6c734c98034acc83392c8
piratepeel <piratepeel@gmail.com>
2017-06-05 19:23:10 +0200

    update test Er and Nr counts of GHRG creation

--- code/test_builds.py
+++ code/test_builds.py
@@ -15,11 +15,13 @@ def test_create_degree_from_snr_av_deg():
 def test_ErNr_in_create2paramGHRG():
     n=1000
     snr=2
-    av_deg=15
+    av_degree=15
     groups_per_level = 2
     n_levels = 2
-    Nr=n*(n-1)
-    D=GHRGbuild.create2paramGHRG(n,snr,av_deg,n_levels,groups_per_level)
+    
+    a,b = GHRGbuild.calculateDegreesFromAvDegAndSNR(snr,av_degree,groups_per_level)
+    
+    D=GHRGbuild.create2paramGHRG(n,snr,av_degree,n_levels,groups_per_level)
     
     # top level
     nl=n/groups_per_level
@@ -29,8 +31,10 @@ def test_ErNr_in_create2paramGHRG():
     assert np.tril(D.node[v]['Er']).sum() == 0
     assert np.tril(D.node[v]['Nr']).sum() == 0
     assert D.node[v]['Nr'][0,1] == nl*nl
+    assert D.node[v]['Er'][0,1] == b/n*nl*nl
     
     # first level
+    n=nl
     nl=nl/groups_per_level
     for v in [1,2]:
         print "node", v
@@ -38,6 +42,7 @@ def test_ErNr_in_create2paramGHRG():
         assert np.tril(D.node[v]['Er']).sum() == 0
         assert np.tril(D.node[v]['Nr']).sum() == 0
         assert D.node[v]['Nr'][0,1] == nl*nl
+        assert D.node[v]['Er'][0,1] == b/n*nl*nl
     
     # second level (leaves)
     for v in xrange(3,7):
@@ -45,3 +50,5 @@ def test_ErNr_in_create2paramGHRG():
         assert np.sum(D.node[v]['Er']>D.node[v]['Nr']) == 0
         print  D.node[v]['Nr'][0,0], nl*(nl-1)/2
         assert D.node[v]['Nr'][0,0] == nl*(nl-1)/2
+        assert D.node[v]['Er'][0,0] == a/n*nl*(nl-1)/2
+        
