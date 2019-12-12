fix small detail in projection error computation

code/experiment_random_projection_errors.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy
 import scipy.linalg
 import GHRGbuild
-import spectral_algorithms as spectral
+import spectral_algorithms_new as spectral
 
 from matplotlib import pyplot as plt
 from scipy.stats import ortho_group
@@ -55,19 +55,20 @@ def calculate_proj_error(U, V, norm):
     proj1 = project_orthogonal_to(U, V)
 
     if norm == 'F':
-        error = scipy.linalg.norm(proj1)
+        error = scipy.linalg.norm(proj1)**2
     elif norm == '2':
         error = scipy.linalg.norm(proj1, 2)
 
     return error
 
-def test_random_projection(n=9):
+def test_random_projection(n=10):
     """Within an n dimensional space, project an orthogonal set of k vectors (a k-dimensional subspace) into a space of dimension n-k
     where 1 vector is shared"""
+    plt.close('all')
 
-    nsamples = 100
+    nsamples = 1000
 
-    testdim = np.arange(n+1)
+    testdim = np.arange(1,n+1)
     print testdim
     meanerror = np.zeros(testdim.size)
     stderror = np.zeros(testdim.size)
@@ -75,31 +76,34 @@ def test_random_projection(n=9):
     for (j, k) in enumerate(testdim):
         # create an orthogonal set of random vectors
         U = ortho_group.rvs(dim=n)
-        U = U[:, :k]
+        U2 = U[:, :k]
         error = np.zeros(nsamples)
-        if k==0:
+        if k<=1:
             continue
         for i in range(nsamples):
-            # create k orthogonal vectors
-            V = ortho_group.rvs(dim=n)
-            V = V[:, :k]
-            V[:,0] = U[:,0]
-            error[i] = calculate_proj_error(U, V, 'F')
+            Q = ortho_group.rvs(dim=n-1)
+            V = U[:,1:].dot(Q)
+            V2 = np.hstack((U[:,0:1],V))[:,:k]
+            error[i] = calculate_proj_error(U2, V2, 'F')
         meanerror[j] = np.mean(error)
         stderror[j] = np.std(error)
 
-    expected_error = np.sqrt((testdim-1) * (n-testdim) /n)
+    expected_error2 = (testdim-1) * (n-testdim) /(n-1)
     plt.figure()
     plt.errorbar(testdim, meanerror, stderror)
-    plt.errorbar(testdim, expected_error)
+    plt.errorbar(testdim, expected_error2)
+    plt.show()
 
-def test_random_projection2(n=9):
-    """Within an n dimensional space, project an orthogonal set of k vectors (a k-dimensional subspace) into a space of dimension p
-    where 1 vector is shared"""
+def test_random_projection2(n=10):
+    """Within an n dimensional space, project an orthogonal set of k vectors (a k-dimensional subspace) into a space of dimension n-k
+    where 1 vector is shared
+    Hierarchical version: make the two subspaces align at the first vector and the fourth
+    """
+    plt.close('all')
 
-    nsamples = 1
-    m=3
-    testdim = np.arange(m+1)
+    nsamples = 1000
+
+    testdim = np.arange(1,n+1)
     print testdim
     meanerror = np.zeros(testdim.size)
     stderror = np.zeros(testdim.size)
@@ -107,20 +111,31 @@ def test_random_projection2(n=9):
     for (j, k) in enumerate(testdim):
         # create an orthogonal set of random vectors
         U = ortho_group.rvs(dim=n)
-        U = U[:, :m]
+        U2 = U[:, :k]
         error = np.zeros(nsamples)
-        print U
-        if k==0:
+        if k<=1:
             continue
         for i in range(nsamples):
-            # create k orthogonal vectors
-            V = U[:, :k]
-            print V
-            error[i] = calculate_proj_error(U, V, 'F')
+            Q = ortho_group.rvs(dim=2)
+            V1 = U[:,1:3].dot(Q)
+            Q = ortho_group.rvs(dim=n-3)
+            Vend = U[:,3:].dot(Q)
+            V2 = np.hstack((U[:,0:1],V1,Vend))[:,:k]
+            error[i] = calculate_proj_error(U2, V2, 'F')
         meanerror[j] = np.mean(error)
         stderror[j] = np.std(error)
 
-    expected_error = np.sqrt((testdim-1) * (n-testdim) /n)
+    levels = [1,3,n]
+    start_end_pairs = zip(levels[:-1],levels[1:])
+
+    expected_error = []
+    for i,j in start_end_pairs:
+        Ks = np.arange(i,j)
+        errors = (Ks - i) * (j - Ks) / (j-i)
+        expected_error = np.hstack([expected_error,errors])
+    expected_error2 = np.hstack([expected_error,0])
+
     plt.figure()
     plt.errorbar(testdim, meanerror, stderror)
-    plt.errorbar(testdim, expected_error)
+    plt.errorbar(testdim, expected_error2)
+    plt.show()

code/spectral_algorithms_new.py
@@ -440,7 +440,7 @@ def expected_errors_random_projection(dim_n,levels):
     expected_error = []
     for i,j in start_end_pairs:
         Ks = np.arange(i,j)
-        errors = (Ks - i) * (j - Ks) / dim_n
+        errors = (Ks - i) * (j - Ks) / (j-i)
         expected_error = np.hstack([expected_error,errors])
     expected_error = np.hstack([expected_error,0])
     return expected_error
@@ -501,6 +501,9 @@ def find_all_relevant_minima_from_errors(errors,std_errors,list_candidate_agglom
         levels = levels + [next_level]
         levels.sort()
         expected_error = expected_errors_random_projection(errors.size, levels)
+        plt.figure(222)
+        Ks = np.arange(expected_error.size) + 1
+        plt.plot(Ks,expected_error)
         next_level, _ = find_smallest_relevant_minima_from_errors(errors, std_errors,expected_error)
 
     print "list_candidate_agglomeration"